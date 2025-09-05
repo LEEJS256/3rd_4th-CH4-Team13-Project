10e07f52b22bdd199e08409788642956b0d25529
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2025-09-05 21:27:57 +0900

    Add: 첫 커밋

---
+++ Source/TFD/TFD.Build.cs
@@ -0,0 +1,31 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class TFD : ModuleRules
+{
+	public TFD(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] 
+		{
+			"Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput"
+				
+				// UI
+				,"UMG", "Slate", "SlateCore", 
+			    //GAS
+				"GameplayAbilities",      // 추가!
+                "GameplayTags",           // 추가!
+                "GameplayTasks"           // 추가!
+		});
+		
+		//PublicIncludePaths.AddRange(new string[] { "TFD" });
+
+		PublicIncludePaths.AddRange(new string[] { 
+			ModuleDirectory + "/../TFD"
+		});
+		
+		
+	}
+}
